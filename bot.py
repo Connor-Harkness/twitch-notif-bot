c9b9c3f80d421f81f5873931b00edf056589589f
Connor_Harkness <connor-harkness@outlook.com>
2022-12-04 13:45:42 +0000

    Add watchlist managment, and a requirements.txt

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
-# This example requires the 'members' privileged intent to use the Member converter
-# and the 'message_content' privileged intent for prefixed commands.
-
-import random
 import config
-import sys, traceback
+import sys
+import traceback
 import datetime
 from cogs.utils import context
 
@@ -11,8 +8,7 @@ import discord
 from discord.ext import commands
 
 description = """
-An example bot to showcase the discord.ext.commands extension module.
-There are a number of utility commands being showcased here.
+Twitch notification bot for Discord
 """
 
 intents = discord.Intents.default()
@@ -23,17 +19,18 @@ intents.message_content = True
 class Twitch_Notif_Bot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix=commands.when_mentioned_or("!"),
+            command_prefix=commands.when_mentioned_or(config.prefix),
             description=description,
             intents=intents,
-            )
+        )
         self.load_cogs()
+
     def load_cogs(self):
         for cog in config.base_cogs:
             try:
                 self.load_extension(cog)
             except Exception as e:
-                print(f"Cog '{cog}' failed to load.", file=sys.stderr)
+                print(f"Cog '{cog}' failed to load.\n{e}", file=sys.stderr)
                 traceback.print_exc()
 
     async def on_ready(self):
@@ -68,7 +65,8 @@ class Twitch_Notif_Bot(commands.Bot):
         elif isinstance(error, commands.CommandInvokeError):
             print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
             traceback.print_tb(error.original.__traceback__)
-            print(f'{error.original.__class__.__name__}: {error.original}', file=sys.stderr)
+            print(
+                f'{error.original.__class__.__name__}: {error.original}', file=sys.stderr)
 
     @property
     def config(self):
@@ -76,6 +74,8 @@ class Twitch_Notif_Bot(commands.Bot):
 
     def run(self):
         super().run(config.token)
+
+
 if __name__ == '__main__':
     twitch_notif_bot = Twitch_Notif_Bot()
     twitch_notif_bot.run()
--- cogs/twitch-notifs.py
+++ cogs/twitch-notifs.py
@@ -1,6 +1,7 @@
 import re
 
 from discord.ext import commands
+import json
 
 
 class Dev(commands.Cog):
@@ -8,14 +9,51 @@ class Dev(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.issue = re.compile(r'##(?P<number>[0-9]+)')
+        self.load_json_storage()
 
-    async def __local_check(self, ctx):
-        return await self.bot.is_owner(ctx.author) or ctx.author.id == 211238461682876416
-        
-    @commands.command()
-    async def add(self, ctx, left: int, right: int):
-        """Adds two numbers together."""
-        await ctx.send(str(left + right))
+    def load_json_storage(self):
+        with open("./data.json") as f:
+            self.data = json.loads(f.read())
+            self.data = self.data["notifications"]
+
+    async def save_data(self):
+        with open("./data.json", "w") as f:
+            self.new_data = {}
+            self.new_data["notifications"] = list(set(self.data))
+            print(self.new_data)
+            f.write(json.dumps(self.new_data))
+
+    @commands.group(name="notifications")
+    async def notifications(self, ctx):
+        """Manage all stream notifications here"""
+        if ctx.invoked_subcommand is None:
+            await (ctx.send('Invaid sub-command passed'))
+
+    @notifications.command()
+    async def add(self, ctx, streamer_username: str):
+        """Adds a streamer to the automatic notifications."""
+        try:
+            self.data.append(streamer_username)
+            await self.save_data()
+            await ctx.send(f"Successfully added {streamer_username} to the watchlist, and saved the data file")
+        except Exception as e:
+            await ctx.send(f"Failed to add {streamer_username} to the watchlist because of \n{e}")
+
+    @notifications.command()
+    async def remove(self, ctx, streamer_username: str):
+        """Removes a streamer to the automatic notifications."""
+        try:
+            self.data.pop(self.data.index(streamer_username))
+            await self.save_data()
+            await ctx.send(f"Successfully removed {streamer_username} from the watchlist, and saved the data file")
+        except Exception as e:
+            await ctx.send(f"Failed to remove {streamer_username} from the watchlist because of \n{e}")
+
+    @notifications.command()
+    async def list(self, ctx):
+        """Lists all the channels currently in the watchlist"""
+        await ctx.send(f"The current watchlist is: {self.data}")
+
+
 def setup(bot):
     bot.add_cog(Dev(bot))
